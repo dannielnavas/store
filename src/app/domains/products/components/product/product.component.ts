df5cdcefbcd217b30507d0e88922239ab9a17205
DannielNavas <dannielnavas@gmail.com>
2023-11-22 07:25:46 -0500

    ✨ Update product and list components

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-product',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './product.component.html',
-  styleUrl: './product.component.css'
+  styleUrl: './product.component.css',
 })
 export class ProductComponent {
-
+  image = `https://picsum.photos/640/640?r=${Math.random()}`;
 }
