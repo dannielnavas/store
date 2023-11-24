2dea5388f6319ae551c193abed88d4128d28268c
DannielNavas <dannielnavas@gmail.com>
2023-11-24 06:56:32 -0500

    ✨ Refactor product list layout

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
 import { Product } from '../../../shared/models/product.model';
 import { ProductComponent } from '../../components/product/product.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, ProductComponent],
+  imports: [CommonModule, ProductComponent, HeaderComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css',
 })
@@ -20,7 +21,7 @@ export class ListComponent {
         title: 'Product 1',
         price: 100,
         description: 'This is the product 1 description',
-        image: 'https://picsum.photos/200/300?random=1',
+        image: 'https://picsum.photos/600/600?random=1',
         creationAt: new Date().toISOString(),
       },
       {
@@ -28,7 +29,7 @@ export class ListComponent {
         title: 'Product 2',
         price: 200,
         description: 'This is the product 2 description',
-        image: 'https://picsum.photos/200/300?random=2',
+        image: 'https://picsum.photos/600/600?random=2',
         creationAt: new Date().toISOString(),
       },
       {
@@ -36,7 +37,7 @@ export class ListComponent {
         title: 'Product 3',
         price: 300,
         description: 'This is the product 3 description',
-        image: 'https://picsum.photos/200/300?random=3',
+        image: 'https://picsum.photos/600/600?random=3',
         creationAt: new Date().toISOString(),
       },
     ];
