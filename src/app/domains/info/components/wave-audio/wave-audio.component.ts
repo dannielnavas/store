2a9fb2cdc1d30abb7f02d3e791020962e06545a6
DannielNavas <dannielnavas@gmail.com>
2023-11-24 06:34:42 -0500

    ✨ Add play/pause functionality to wave audio component

--- src/app/domains/info/components/wave-audio/wave-audio.component.ts
+++ src/app/domains/info/components/wave-audio/wave-audio.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild, signal } from '@angular/core';
 
 import WaveSurfer from 'wavesurfer.js';
 
@@ -8,16 +8,24 @@ import WaveSurfer from 'wavesurfer.js';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './wave-audio.component.html',
-  styleUrl: './wave-audio.component.css'
+  styleUrl: './wave-audio.component.css',
 })
 export class WaveAudioComponent {
   @Input({ required: true }) url!: string;
-  @ViewChild('waveform') waveform!: ElementRef;
+  @ViewChild('wave') waveform!: ElementRef;
+  private ws!: WaveSurfer;
+  isPlaying= signal(false); // signal is a function that returns a signal
 
   ngAfterViewInit() {
-    WaveSurfer.create({
+    this.ws = WaveSurfer.create({
       url: this.url,
       container: this.waveform.nativeElement,
-    })
+    });
+    this.ws.on('play', () => this.isPlaying = signal(true));
+    this.ws.on('pause', () => (this.isPlaying = signal(false)));
+  }
+
+  playPause() {
+    this.ws.playPause();
   }
 }
