6afef2b7466cf308af1a359732734f03eebb4d5c
DannielNavas <dannielnavas@gmail.com>
2023-11-24 05:43:29 -0500

    ✨ Fix input typo and add lifecycle hooks

--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, Input, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './counter.component.html',
-  styleUrl: './counter.component.css'
+  styleUrl: './counter.component.css',
 })
 export class CounterComponent {
+  @Input({ required: true }) duration: number = 0;
+  @Input({ required: true }) messagges: string = '';
 
+  construntor() {
+    // No async code here
+    // before rendering
+    console.log('constructor');
+    console.log('-'.repeat(50));
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    // before and during rendering
+    console.log('ngOnChanges');
+    console.log(changes);
+    console.log('-'.repeat(50));
+  }
 }
