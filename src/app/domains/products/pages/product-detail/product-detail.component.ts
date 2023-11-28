becac506b9fc2fcaaec473b7dd104d5236b6f5ce
DannielNavas <dannielnavas@gmail.com>
2023-11-28 06:20:05 -0500

    ✨ Refactor product detail component to dynamically display images

--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -14,17 +14,25 @@ export class ProductDetailComponent {
   @Input() id?: string;
   private productServices = inject(ProductService);
   product = signal<Product | null>(null);
+  cover = signal<string>('');
 
   ngOnInit() {
     if (this.id) {
       this.productServices.getOne(this.id).subscribe({
         next: (product) => {
           this.product.set(product);
+          if(product?.images && product.images.length > 0) {
+            this.cover.set(product.images[0]);
+          }
         },
         error: (err) => {
           console.log(err);
         },
       });
     }
+  }
+
+  changeCover(newImages: string) {
+    this.cover.set(newImages);
   }
 }
