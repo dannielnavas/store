2a4b33153d035570fc7fa49019e73ea9e1eac264
DannielNavas <dannielnavas@gmail.com>
2023-11-28 06:13:55 -0500

    ✨ Refactor product detail component to use dynamic data

--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, Input, inject, signal } from '@angular/core';
+import { Product } from '@shared/models/product.model';
+import { ProductService } from '@shared/services/product.service';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './product-detail.component.html',
-  styleUrl: './product-detail.component.css'
+  styleUrl: './product-detail.component.css',
 })
 export class ProductDetailComponent {
+  @Input() id?: string;
+  private productServices = inject(ProductService);
+  product = signal<Product | null>(null);
 
+  ngOnInit() {
+    if (this.id) {
+      this.productServices.getOne(this.id).subscribe({
+        next: (product) => {
+          this.product.set(product);
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+    }
+  }
 }
