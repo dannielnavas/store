0319cb963a58074dadb088dec7c3aea15631465c
DannielNavas <dannielnavas@gmail.com>
2023-11-28 06:33:34 -0500

    ✨ Refactor product list component and add category functionality

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, signal } from '@angular/core';
 import { HeaderComponent } from '@shared/components/header/header.component';
-import { Product } from '@shared/models/product.model';
+import { Category, Product } from '@shared/models/product.model';
 import { CartService } from '@shared/services/cart.service';
+import { CategoryService } from '@shared/services/category.service';
 import { ProductService } from '@shared/services/product.service';
 import { ProductComponent } from '../../components/product/product.component';
 
@@ -15,17 +16,30 @@ import { ProductComponent } from '../../components/product/product.component';
 })
 export class ListComponent {
   products = signal<Product[]>([]);
+  categories = signal<Category[]>([]);
   private cartService = inject(CartService);
   private productsService = inject(ProductService);
-
+  private categoryService = inject(CategoryService);
 
   ngOnInit() {
     this.getProducts();
+    this.getCategories();
   }
 
-  getProducts() {
+  private getProducts() {
     this.productsService.getProducts().subscribe((products) => {
       this.products.set(products);
+    });
+  }
+
+  private getCategories() {
+    this.categoryService.getAllCategories().subscribe({
+      next: (categories) => {
+        this.categories.set(categories);
+      },
+      error: (err) => {
+        console.log(err);
+      },
     });
   }
 
