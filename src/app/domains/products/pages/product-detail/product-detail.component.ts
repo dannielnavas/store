c1e05613b6acfc66a60543bf42029c48811dfe58
Danniel Navas <dannielnavas@gmail.com>
2025-04-01 07:35:45 -0500

    ✨ Mejorar el componente 'product-detail' al actualizar la lógica de la imagen de portada utilizando 'linkedSignal' para una mejor reactividad.

--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -1,5 +1,12 @@
 import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { Component, OnInit, inject, signal, input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  inject,
+  input,
+  linkedSignal,
+  signal,
+} from '@angular/core';
 import { Product } from '@shared/models/product.model';
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
@@ -15,7 +22,21 @@ export default class ProductDetailComponent implements OnInit {
   private productServices = inject(ProductService);
   private cartService = inject(CartService);
   product = signal<Product | null>(null);
-  cover = signal<string>('');
+  // cover = signal<string>('');
+  // los computed casi no se pueden modificar
+  // cover = computed(() => {
+  //   return this.product()?.images[0] || '';
+  // });
+  // $cover = linkedSignal(() => {
+  //   return this.product()?.images[0] || '';
+  // });
+
+  $cover = linkedSignal({
+    source: this.product,
+    computation: (product, previosValue) => {
+      return product?.images[0] || previosValue?.value;
+    },
+  });
 
   ngOnInit() {
     const slug = this.slug();
@@ -23,9 +44,9 @@ export default class ProductDetailComponent implements OnInit {
       this.productServices.getOneBySlug(slug).subscribe({
         next: product => {
           this.product.set(product);
-          if (product?.images && product.images.length > 0) {
-            this.cover.set(product.images[0]);
-          }
+          // if (product?.images && product.images.length > 0) {
+          //   this.cover.set(product.images[0]);
+          // }
         },
         error: err => {
           console.log(err);
@@ -35,7 +56,7 @@ export default class ProductDetailComponent implements OnInit {
   }
 
   changeCover(newImages: string) {
-    this.cover.set(newImages);
+    this.$cover.set(newImages);
   }
 
   addToCart() {
