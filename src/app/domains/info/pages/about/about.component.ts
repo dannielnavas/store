606b8021717b7da52968cfe69d7d9f47f30eac56
Danniel Navas <dannielnavas@gmail.com>
2025-04-02 07:20:12 -0500

    ✨ Mejorar el componente 'about' al agregar señales reactivas para la emisión de valores iniciales y sin inicial, mejorando la interactividad.

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { FormsModule } from '@angular/forms';
 import { CounterComponent } from '@shared/components/counter/counter.component';
 import { HighlightDirective } from '@shared/directives/highlight.directive';
+import { BehaviorSubject, delay, Subject } from 'rxjs';
 import { WaveAudioComponent } from '../../components/wave-audio/wave-audio.component';
 
 @Component({
@@ -21,6 +23,22 @@ export default class AboutComponent {
   duration = signal(1000);
   messages = signal('Danniel');
 
+  // observables convertir a signal
+  //   Casos donde NO deberías usar toSignal()
+  // Si el observable no tiene un valor inicial y no puedes definirlo correctamente.
+  // Cuando necesitas operadores avanzados de RxJS como switchMap, mergeMap o combineLatest.
+  // Cuando manejas streams de eventos intensivos, como WebRTC o sensores, ya que Signals no están optimizados para eventos de alto rendimiento.
+
+  obsWithInit$ = new BehaviorSubject<string>('Initial value');
+  $withInit = toSignal(this.obsWithInit$, {
+    requireSync: true, // que sea sincrono
+  });
+
+  obsWithoutInit$ = new Subject<string>();
+  $withoutInit = toSignal(this.obsWithoutInit$.pipe(delay(3000)), {
+    initialValue: '>>>> Initial value',
+  });
+
   changeDuration(event: Event) {
     const input = event.target as HTMLInputElement;
     this.duration = signal(Number(input.value));
@@ -29,5 +47,12 @@ export default class AboutComponent {
   changeMessages(event: Event) {
     const input = event.target as HTMLInputElement;
     this.messages = signal(input.value);
+  }
+
+  emitWithInit() {
+    this.obsWithInit$.next('New value');
+  }
+  emitWithoutInit() {
+    this.obsWithoutInit$.next('******');
   }
 }
