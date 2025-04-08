b428dfee3ab18580964d1ca23a5224f71eca58ac
Danniel Navas <dannielnavas@gmail.com>
2025-04-08 07:22:20 -0500

    ✨ Agregar rehidratación incremental al cliente en la configuración de la aplicación

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,6 +10,7 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 import {
   provideClientHydration,
   withEventReplay,
+  withIncrementalHydration,
 } from '@angular/platform-browser';
 import { routes } from './app.routes';
 
@@ -22,6 +23,8 @@ export const appConfig: ApplicationConfig = {
       withPreloading(PreloadAllModules)
     ),
     provideHttpClient(withFetch()), // withFetch() es el que se encarga de hacer las peticiones http del lado del servidor
-    provideClientHydration(withEventReplay()), // aquí hace la rehydration del lado del cliente
+    provideClientHydration(withEventReplay(), withIncrementalHydration()), // aquí hace la rehydration del lado del cliente
+    // withEventReplay para que no se pierdan los eventos que se han emitido por el usurio como los clicks
+    // withIncrementalHydration esto hace la rehidratación incremental, es decir, va rehidratando los componentes a medida que se van viendo en la pantalla
   ],
 };
